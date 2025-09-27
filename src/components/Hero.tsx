ea636e5a668cff026101dc39bbe51882c6920779
Niraj Kshirsagar <nirajkshirsagar72@gmail.com>
2025-09-27 19:55:51 +0530

    fix error

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { Star, ArrowRight, CheckCircle } from "lucide-react";
+import { Star, ArrowRight} from "lucide-react";
 import RotatingText from "./RotatingText";
 import { motion } from "framer-motion";
 import Image from "next/image";
