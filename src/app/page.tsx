76ca89212e24fd3d97bf40024432c3b6d171d755
Niraj Kshirsagar <nirajkshirsagar72@gmail.com>
2025-10-25 15:33:52 +0530

    feat(page): enable SSR for dynamic imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,28 @@ import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-const PopularCourses = dynamic(() => import("@/components/Courses"));
-const Features = dynamic(() => import("@/components/Features"));
+const PopularCourses = dynamic(() => import("@/components/Courses"), {
+  ssr: false,
+});
+const Features = dynamic(() => import("@/components/Features"), { ssr: false });
 const KnowledgeDeliverySection = dynamic(
-  () => import("@/components/KnowledgeDeliverySection")
+  () => import("@/components/KnowledgeDeliverySection"),
+  { ssr: false }
 );
-const MonetizeSection = dynamic(() => import("@/components/MonetizeSection"));
+const MonetizeSection = dynamic(() => import("@/components/MonetizeSection"), {
+  ssr: false,
+});
 const CommunicateSection = dynamic(
-  () => import("@/components/CommunicateSection")
+  () => import("@/components/CommunicateSection"),
+  { ssr: false }
 );
 const ExternalTeacherSection = dynamic(
-  () => import("@/components/ExternalTeacherSection")
+  () => import("@/components/ExternalTeacherSection"),
+  { ssr: false }
 );
-const UpscaleHero = dynamic(() => import("@/components/UpscaleHero"));
+const UpscaleHero = dynamic(() => import("@/components/UpscaleHero"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
